5b3ffe43f432ea5dfb7f219bd214a8fd685bc53e
stevenhorkey <stevenhorkey@gmail.com>
2018-02-04 17:41:17 -0700

    got it working as a function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,3 @@
-// Selects h1 and changes color every 0.75 seconds. 
-var headTitle = document.querySelector("h1");
-var isPink = false;
-
-setInterval(function() {
-    if (isPink) {
-        headTitle.style.color = "green";
-    } else {
-        headTitle.style.color = "hotpink";
-    }
-    isPink = !isPink;
-}, 750);
-
 // Game=============================================
 
 // Sets userWins to html #user-wins
@@ -23,14 +10,93 @@ var userGuess = document.getElementById("user-guess");
 var guessRemain = document.getElementById("guess-remain");
 
 // Sets a  word for first game
-var words = {
+var bands = {
     hendrix : {name:'hendrix', solution:['h','e','n','d','r','i','x']},
     santana : {name:'santana', solution:['s','a','n','t']},
     beatles : {name:'beatles', solution:['b','e','a','t','l','s']},
 }
 
+var jimi = bands.hendrix;
+var carlos = bands.santana;
+var fabFour = bands.beatles;
+
+// Alphabet array used to compare user input
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+// Sets initial score to 0
+var wins = 0;
+var won = false;
+// Sets word value on screen from array words
+
+console.log(bands.name);
+// Sets empty arrays for use later
+var guessedLetters = [];
+var currentBandGuess = [];
+    
+
+function gameSelection(band) {
+    currentWord.textContent = band.name;
+    console.log(band.name);
+    // Sets available guesses to string length
+    var guessCount = band.name.length + 1;
+    // Displays intial guesses remaining
+    guessRemain.textContent = guessCount;
+
+    document.onkeyup = function(event) {
+        // Sets variable userInput  equall to key released
+        var userInput = event.key;
+        // Makes all inputs lowercase
+        userInput = userInput.toLowerCase();    
+            
+        // Tests to make sure input is a letter
+        if (letters.includes(userInput)){
+            // Alerts in the console which key was pressed
+            console.log(userInput + " was pressed!")
+           
+            // Checks if the user input is already in guessedLettesr array and if not, appends it.
+            if(!guessedLetters.includes(userInput)) {
+                guessedLetters.push(userInput);
+                userGuess.textContent = guessedLetters;
+                if (guessRemain.textContent > 1 ) {
+                guessRemain.textContent = guessRemain.textContent - 1;
+                } else {
+                    var retry = confirm("SOOOORRY! You failed :( Want to try again?")
+                    if (retry) {
+                        location.reload();
+                    }
+                }
+            } 
+    
+            for(i = 0; i < band.solution.length; i ++) {
+                if (userInput === band.solution[i] && !currentBandGuess.includes(band.solution[i])) {
+                    currentBandGuess.push(band.solution[i]);
+                    console.log(currentBandGuess);
+                } else {
+                    console.log('already includes');
+                }
+            }
+                
+            if (band.solution.length === currentBandGuess.length) {
+                console.log('winning');
+                userWins.textContent = wins + 1;
+            }
+        }
+    }
+}
+
+
+gameSelection(jimi)
+
+
+
+    
+
+
+
+    
+
 // Chooses a random band objcet for the game goal and sets it equall to variable word.
-var word = words.hendrix;
+// var word = words.hendrix;
 // var random = Math.floor(Math.random() * 3)
 // if(random === 0) {
 //     word = words.hendrix;
@@ -39,77 +105,6 @@ var word = words.hendrix;
 // } else {
 //     word = words.beatles;
 // }
-
-
-
-// Alphabet array used to compare user input
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-// Sets initial score to 0
-var wins = 0;
-// Sets word value on screen from array words
-currentWord.textContent = word.name;
-console.log(word.name);
-// Sets empty arrays for use later
-var guessedLetters = [];
-var currentBandGuess = [];
-    
-// Sets available guesses to string length
-var guessCount = word.name.length;
-// Displays intial guesses remaining
-guessRemain.textContent = guessCount;
-
-document.onkeyup = function(event) {
-    // Sets variable userInput  equall to key released
-    var userInput = event.key;
-    // Makes all inputs lowercase
-    userInput = userInput.toLowerCase();    
-        
-    // Tests to make sure input is a letter
-    if (letters.includes(userInput)){
-        // Alerts in the console which key was pressed
-        console.log(userInput + " was pressed!")
-       
-        // Checks if the user input is already in guessedLettesr array and if not, appends it.
-        if(!guessedLetters.includes(userInput)) {
-            guessedLetters.push(userInput);
-            userGuess.textContent = guessedLetters;
-            if (guessRemain.textContent > 1) {
-            guessRemain.textContent = guessRemain.textContent - 1;
-            } else {
-                var retry = confirm("SOOOORRY! You failed :( Want to try again?")
-                if (retry) {
-                    location.reload();
-                }
-            }
-        } 
-
-        for(i = 0; i < word.solution.length; i ++) {
-            if (userInput === word.solution[i] && !currentBandGuess.includes(word.solution[i])) {
-                currentBandGuess.push(word.solution[i]);
-                console.log(currentBandGuess);
-            } else {
-                console.log('already includes');
-            }
-        }
-            
-        if (word.solution.length === currentBandGuess.length) {
-            console.log('winning');
-            userWins.textContent = wins + 1;
-        }
-    }
-}
-
-    
-
-
-
-    
-
-
-
-    
-
 
 
 
